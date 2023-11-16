61b4e05c32e0d4d90e5c97db6d74ddb7aca47f84
Guilherme Fries <142254430+guilimao7@users.noreply.github.com>
2023-11-16 18:09:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+/*console.log("Meu nome é Guilherme")
+
+prompt("Digite seu nome")
+
+const nomePersonagem = Guilherme
+const sobrenomePersonagem = Fries
+const valorIdade = 15
+const empregado = false
+
+console.log("Nome", nomePersonagem, "Sobrenome", sobrenomePersonagem, "Idade", valorIdade, "Emprego", empregado)
+
+let nomePersonagem = prompt("Digite seu nome")
+
+let valorIdade = prompt("Digite sua idade")
+
+console.log(typeof nomePersonagem)
+console.log(typeof valorIdade)
+
+const numero = 123;
+const numeroComoString = numero.toString();
+console.log(typeof numeroComoString); //Saída: "string"
+
+const stringNumerica = "456"
+const stringConvertidaParaNumero = Number(stringNumerica)
+console.log(typeof  stringConvertidaParaNumero);  //  Saída: "number"*/
+
+//-------------------------------------------------------------------------------
+
+/*let nome 
+let cidade 
+
+console.log(typeof nome)
+console.log(typeof cidade)
+
+nomeValor = prompt("Digite seu nome")
+cidadeValor = prompt("Digite o nome da sua cidade")
+
+nome = (nomeValor)
+cidade = (cidadeValor)
+
+console.log(typeof nomeValor)
+console.log(typeof cidadeValor)
+
+console.log("Olá, meu nome é", nomeValor, "e eu moro em", cidadeValor)*/
+
+//-------------------------------------------------------------------------------
+
+perguntaAnimal = prompt("Qual seu animal favorito?")
+perguntaComida = prompt("Qual sua comida favorito?")
+perguntaBebida = prompt("Qual sua bebida favorito?")
+
+console.log("Qual seu animal favorito?", "Meu animal favorito é o(a)", perguntaAnimal)
+console.log("Qual sua comida favorita?", "Minha comida favorita é", perguntaComida)
+console.log("Qual sua bebida favorito?", "Minha bebida favorita é", perguntaBebida)
